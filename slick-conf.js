9e035bc06953a3e793fde6907a98015166df95bd
Willy <willycifuentesalmeida@gmail.com>
2018-10-20 15:16:36 -0500

    Added first files

---
+++ slick-conf.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+      infinite: true,
+        centerMode: true,
+       dots:true,
+      slidesToShow: 4,
+      slidesToScroll: 4
+    }); 
+});
